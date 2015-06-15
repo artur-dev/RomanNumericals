abc3301d3923bbba977b818c40229adb8dc76fc7
Artur <artur.siupik@dm-companies.com>
2015-06-15 11:06:25 +0100

    RomanNumericals

---
+++ RomanNumericals.php
@@ -0,0 +1,16 @@
+<?php
+
+class RomanNumericals {
+    private $number;
+
+    private $conversion_chart = [
+        ['I', 'V', 'X'],
+        ['X', 'L', 'C'],
+        ['C', 'D', 'M'],
+    ];
+
+    public function __construct($number)
+    {
+        $this->number;
+    }
+}
