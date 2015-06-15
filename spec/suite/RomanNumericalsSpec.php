e7c74669805d2c718657be9f629a4bd0b52274a1
Artur <artur.siupik@dm-companies.com>
2015-06-15 11:00:28 +0100

    test

---
+++ spec/suite/RomanNumericalsSpec.php
@@ -0,0 +1,19 @@
+<?php
+//namespace CodeKata\Supermarket\Tests\Models;
+
+use kahlan\plugin\Stub;
+//    CodeKata\Supermarket\Models\UnitPrice;
+
+describe("UnitPrice model", function() {
+    beforeEach(function() {
+        //$this->model = new UnitPrice(['unit_price' => 4.99]);
+    });
+
+    it("returns total price without any discount for multiple units", function() {
+        //expect($this->model->getFormattedTotalPrice(10))->toBe('49.90');
+    });
+
+    it("returns full price for single unit", function() {
+        expect(true)->toBeTruthy();
+    });
+});
