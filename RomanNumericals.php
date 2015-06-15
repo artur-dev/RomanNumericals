63d4d9806e92d6ffd9a9a0fa458907510baaace9
Artur <artur.siupik@dm-companies.com>
2015-06-15 11:31:25 +0100

    1-3

--- RomanNumericals.php
+++ RomanNumericals.php
@@ -5,12 +5,38 @@ class RomanNumericals {
 
     private $conversion_chart = [
         ['I', 'V', 'X'],
-        ['X', 'L', 'C'],
-        ['C', 'D', 'M'],
     ];
 
-    public function __construct($number)
+    public function __construct()
     {
-        $this->number;
+    }
+
+    public function getRoman($number)
+    {
+        return $this->convert($number);
+    }
+
+    private function convertStep($number, $chart)
+    {
+        $s = '';
+
+        switch ($number % 9) {
+            case 0 :
+            case 1 :
+            case 2 :
+            case 3 :
+                for ($i = 1; $i <= $number; $i++) {
+                    $s .= $this->conversion_chart[0];
+                }
+        }
+
+        return $s;
+    }
+
+    private function convert($number)
+    {
+        return
+            $this->convertStep($number % 10, $this->conversion_chart[0]);
+
     }
 }
